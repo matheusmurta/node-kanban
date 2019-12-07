c957db2471c24c1b85dce8ca067c64be63092e70
Matheus Gonçalves Murta <matheus_mge@hotmail.com>
2019-12-07 03:19:09 -0300

    setup swagger documentation of api, generate docs for routes and models

--- server/models/task.js
+++ server/models/task.js
@@ -1,4 +1,33 @@
 'use strict';
+
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      Task:
+ *        type: object
+ *        required:
+ *          - title
+ *        properties:
+ *          title:
+ *            type: string
+ *            description: set an objective title for task.
+ *          stage:
+ *            type: string
+ *            description: task stage, such as todo or done.
+ *          description:
+ *            type: string 
+ *            description: describe the complete task with details.
+ *          priority:
+ *            type: string
+ *            description: number representing the priority level (the lower the number the higher the priority).
+ *        example:
+ *           title: UX Research
+ *           stage: backlog
+ *           description: Designer team meeting with client
+ *           priority: 1
+ */
+
 module.exports = (sequelize, DataTypes) => {
   const Task = sequelize.define('Task', {
     title: DataTypes.STRING,
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 import Boards from '../controllers/board';
 import Tasks from '../controllers/task';
 
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 export default (app) => {
 
     app.get('/api', (req, res) => res.status(200).send({
@@ -8,8 +11,143 @@ export default (app) => {
     }));
 
     app.post('/api/boards', Boards.signUp);
+
+    /**
+     * @swagger
+     * tags:
+     *   name: Kanban App 
+     *   description: Tasks / Progress management
+     */
+
+    /**
+     * @swagger
+     * path:
+     *  /tasks/:
+     *    post:
+     *      summary: Create a new task
+     *      tags: [Tasks]
+     *      requestBody:
+     *        required: true
+     *        content:
+     *          application/json:
+     *            schema:
+     *              $ref: '#/components/schemas/Task'
+     *      responses:
+     *        "200":
+     *          description: A task schema
+     *          content:
+     *            application/json:
+     *              schema:
+     *                $ref: '#/components/schemas/Task'
+     */
     app.post('/api/boards/:boardId/tasks', Tasks.create);
+
+    /**
+     * @swagger
+     * path:
+     *  /tasks/:
+     *    get:
+     *      summary: Get all tasks
+     *      tags: [Tasks]
+     *      responses:
+     *        "200":
+     *          description: An array of tasks
+     *          content:
+     *            application/json:
+     *              schema:
+     *                $ref: '#/components/schemas/Task'
+     */
     app.get('/api/tasks', Tasks.list);
+
+    /**
+    * @swagger
+    * tags:
+    *   name: Tasks
+    *   description: Tasks management
+    */
+
+    /**
+     * @swagger
+     * path:
+     *  /tasks/:
+     *    put:
+     *      summary: Update task
+     *      tags: [Tasks]
+     *      requestBody:
+     *        required: true
+     *        content:
+     *          application/json:
+     *            schema:
+     *              $ref: '#/components/schemas/Task'
+     *      responses:
+     *        "200":
+     *          description: A task schema
+     *          content:
+     *            application/json:
+     *              schema:
+     *                $ref: '#/components/schemas/Task'
+     */
     app.put('/api/tasks/:taskId', Tasks.modify);
+
+    /**
+     * @swagger
+     * path:
+     *  /tasks/:
+     *    delete:
+     *      summary: Update task
+     *      tags: [Tasks]
+     *      requestBody:
+     *        required: true
+     *        content:
+     *          application/json:
+     *            schema:
+     *              $ref: '#/components/schemas/Task'
+     *      responses:
+     *        "200":
+     *          description: A task schema
+     *          content:
+     *            application/json:
+     *              schema:
+     *                $ref: '#/components/schemas/Task'
+     */
     app.delete('/api/tasks/:taskId', Tasks.delete);
+
+    // Swagger set up
+    const options = {
+        swaggerDefinition: {
+            openapi: "3.0.0",
+            info: {
+                title: "Document Express API Kanban APP",
+                version: "1.0.0",
+                description:
+                    "document and Express API",
+                license: {
+                    name: "MIT",
+                    url: "https://choosealicense.com/licenses/mit/"
+                },
+                contact: {
+                    name: "Swagger",
+                    url: "https://swagger.io",
+                    email: "matheus_mge@hotmail.com"
+                }
+            },
+            servers: [
+                {
+                    url: "http://localhost:3000"
+                }
+            ]
+        },
+        apis: [
+            "./server/models/Task.js",
+            "./server/routes/index.js",
+        ]
+    };
+    const specs = swaggerJsdoc(options);
+    app.use("/docs", swaggerUi.serve);
+    app.get(
+        "/docs",
+        swaggerUi.setup(specs, {
+            explorer: true
+        })
+    );
 };
